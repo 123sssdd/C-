507cd32f607005ef1aee8cbf13ff0131ca9cbba2
123sssdd <1253201093@qq.com>
2025-12-04 18:35:02 +0800

    qq

---
+++ Hospital/Hospital/Controllers/HomeController.cs
@@ -0,0 +1,124 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Hospital.Models;
+
+namespace Hospital.Controllers
+{
+    public class HomeController : Controller
+    {
+        HospitalDBEntities db = new HospitalDBEntities();
+        // GET: Home
+        public ActionResult Index()
+        {
+            ViewBag.zaizhi =db.Doctor.Where(n=>n.Ystate=="在职").Count();
+            ViewBag.lizhi= db.Doctor.Where(n => n.Ystate == "已离职").Count();
+            ViewBag.xiujia = db.Doctor.Where(n => n.Ystate == "休假").Count();
+            ViewBag.zhuyuan = db.Zhuyuan.Where(n => n.state == "已入院").Count();
+            ViewBag.meng = db.Guahao.ToList().Count();//挂号人数
+            ViewBag.h = db.Guahao.Where(n=>n.Gstate=="0").ToList();//今天挂号
+            ViewBag.k = db.Bumen.ToList().Count;
+            ViewBag.z = db.Guahao.Where(n => n.Gtime.Month == DateTime.Now.Month).ToList().Count();
+          var y = db.Doctor.ToList();
+            return View(y);
+        }
+        //登录的方法
+        public ActionResult Login()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Login(Login login)
+        {
+            var p = db.Login.FirstOrDefault(n => n.Account == login.Account && n.Password == login.Password && n.Doctor.Ystate!= "已离职");
+            if (p!=null)
+            {
+                Session["name"] = p.Doctor.Yname;//医生姓名
+                Session["zhiwu"] = p.Zhiwu;//医生职务
+                Session["Yid"] = p.YsID;//医生ID
+                return Redirect("/Home/Index");
+            }
+            else
+            {
+                return Content("<script> alert('登录失败！账号或密码错误！');window.location.href='/Home/Login';</script > ");
+            }
+        }
+        /// <summary>
+        /// 退出登录
+        /// </summary>
+        /// <returns></returns>
+        public ActionResult LogOut()
+        {
+            Session.Remove("name");
+            return RedirectToAction("Login");
+        }
+        /// <summary>
+        /// 用户管理模块
+        /// </summary>
+        /// <returns></returns>
+        public ActionResult Yhu()
+        {
+            var p = db.Login.ToList();
+            ViewBag.li = db.Doctor.ToList();
+            return View(p);
+        }
+        [HttpPost]
+        public ActionResult Yhu(Login l)
+        {
+            var p = db.Login.ToList();
+            ViewBag.li = db.Doctor.ToList();         
+            db.Login.Add(l);           
+            if (db.SaveChanges()>0)
+            {
+                return RedirectToRoute(p);
+            }
+            else
+            {
+                return Content("<script> alert('添加失败请重试！');window.location.href='/Home/Yhu';</script > ");
+            }
+           
+        }
+        /// <summary>
+        /// 用户删除方法
+        /// </summary>
+        /// <param name="ac"></param>
+        /// <returns></returns>
+        public ActionResult Del(int ac)
+        {
+            var p = db.Login.Find(ac);
+            db.Login.Remove(p);
+            if (db.SaveChanges()>0)
+            {
+                return Content("yes");
+            }
+            else
+            {
+                return Content("no");
+            }
+        }
+        /// <summary>
+        /// 修改方法
+        /// </summary>
+        /// <param name="ID"></param>
+        /// <param name="Account"></param>
+        /// <param name="Password"></param>
+        /// <returns></returns>
+        public ActionResult Xiu(int ID ,string Account,string Password)
+        {
+            var p= db.Login.Find(ID);
+            p.Account = Account;
+            p.Password = Password;
+            if (db.SaveChanges() > 0)
+            {
+                return Content("yes");
+            }
+            else
+            {
+                return Content("no");
+            }
+        }
+
+    }
+}
---
+++ Hospital/Hospital/Controllers/ZhuController.cs
@@ -0,0 +1,137 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Hospital.Models;
+
+namespace Hospital.Controllers
+{
+    public class ZhuController : Controller
+    {
+        HospitalDBEntities db = new HospitalDBEntities();
+        // GET: Zhu
+        public ActionResult Index()
+        {
+            var p = db.Zhuyuan.Where(n => n.state == "未入院").ToList();
+            ViewBag.h= db.Zhuyuan.Where(n => n.state == "已入院").ToList();
+            return View(p);
+        }
+        /// <summary>
+        /// 入院登记
+        /// </summary>
+        /// <param name="chuang"></param>
+        /// <param name="money"></param>
+        /// <returns></returns>
+        public ActionResult Tian(int Zhuid, int chuang,decimal money)
+        {
+            ViewBag.h = db.Zhuyuan.Where(n => n.state == "已入院").ToList();
+            var zh = db.Zhuyuan.Find(Zhuid);
+            zh.Chuang = chuang;
+            zh.money = money;
+            zh.riqi = DateTime.Now;
+            zh.state = "已入院";
+            if (db.SaveChanges()>0)
+            {
+                return Content("yes");
+            }
+            else
+            {
+                return Content("no");
+            }
+            
+        }
+        /// <summary>
+        /// 出院结算
+        /// </summary>
+        /// <param name="Zhuid"></param>
+        /// <param name="money"></param>
+        /// <returns></returns>
+        public ActionResult Chu( int Zhuid,decimal money)
+        {
+            var zh = db.Zhuyuan.Find(Zhuid);
+            zh.moneyz = money;
+            zh.riqic = DateTime.Now;
+            zh.state = "已出院";
+            if (db.SaveChanges() > 0)
+            {
+                return Content("yes");
+            }
+            else
+            {
+                return Content("no");
+            }
+        }
+        /// <summary>
+        /// 住院处方划价
+        /// </summary>
+        /// <returns></returns>
+        public ActionResult Zhuchu()
+        {
+            ViewBag.Yao = db.Medicine.Where(n => n.Mguoqi > DateTime.Now);
+            var p= db.Zhuyuan.Where(n => n.state == "已入院").ToList();
+            return View(p);
+        }
+        /// <summary>
+        /// 添加药品到处方表
+        /// </summary>
+        /// <param name="id"></param>
+        /// <returns></returns>
+        [HttpPost]
+        public ActionResult Buy(int id)
+        {
+            db.Configuration.ProxyCreationEnabled = false;//贪婪加载
+            var data = db.Medicine.SingleOrDefault(n => n.medicineID == id);
+            return Json(new { code = 0, data = data });
+        }
+        /// <summary>
+        /// 处方表提交方法
+        /// </summary>
+        /// <param name="list"></param>
+        /// <returns></returns>
+        [HttpPost]
+        public ActionResult submit(List<test> list)
+        {
+            List<Morder> moList = new List<Morder>();
+            foreach (var item in list)
+            {
+                //接收传过来的值（药品ID，购买的药品数量添加到订单表）
+                Morder morder = new Morder { medicineID = item.ids, Mshu = item.num, Gid = item.gid, state = item.state, zhifu = item.zhifu, mhz = item.mhz };
+                //改变药品表的药品数量
+                var p = db.Medicine.Find(item.ids);
+                if (p != null)
+                {
+                    p.Mshu = p.Mshu - item.num;
+                }
+                moList.Add(morder);
+            }
+            db.Morder.AddRange(moList);
+            db.SaveChanges();
+            return Json(new { code = 0 });
+        }
+        /// <summary>
+        /// 住院患者库
+        /// </summary>
+        /// <returns></returns>
+        public ActionResult Huan()
+        {
+            var h = db.Zhuyuan.ToList();
+            return View(h);
+        }
+        [HttpPost]
+        public ActionResult Huan(string name)
+        {
+            var h = db.Zhuyuan.Where(n=>n.Guahao.Gname.Contains(name)).ToList();
+            return View(h);
+        }
+    }
+    public class test1
+    {
+        public int ids { get; set; }
+        public int num { get; set; }
+        public int gid { get; set; }//挂号id
+        public string state { get; set; }
+        public string zhifu { get; set; }
+        public string mhz { get; set; }
+    }
+}
